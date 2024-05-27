1b5d246b04400c7a4c235ce281244654c19ff26e
Jan Putra <61097382+Janinh00@users.noreply.github.com>
2024-05-27 14:20:16 +0700

    adds

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -3,48 +3,48 @@ import pahalaaqiqahImage from '../assets/pahalaaqiqah1.png'; // Import the image
 import syshabImage from '../assets/syshab.png'; // Import the image using require or import
 import codeassesmentImage from '../assets/codeassesment.png'; // Import the image using require or import
 
-
 const projectsData = [
   {
     title: 'Project One',
     image: pahalaaqiqahImage,
     githubLink: 'https://github.com/',
-    liveDemoLink: 'https://github.com/'
+    liveDemoLink: 'https://pahalaaqiqah.com/order/'
   },
   {
     title: 'Project Two',
     image: syshabImage,
     githubLink: 'https://github.com/',
-    liveDemoLink: 'https://github.com/'
+    liveDemoLink: '#'
   },
  {
     title: 'Project Three',
     image: codeassesmentImage,
     githubLink: 'https://github.com/',
-    liveDemoLink: 'https://github.com/'
+    liveDemoLink: 'https://janinh00.github.io/code-assessment/'
   }
 ];
 
 const Project = ({ title, image, githubLink, liveDemoLink }) => (
   <div className="details-container color-container">
     <div className="article-container">
-      <img src={image} alt={title} className="project-img" />
+      <a href={liveDemoLink} target="_blank" rel="noopener noreferrer">
+        <img src={image} alt={title} className="project-img" />
+      </a>
     </div>
     <h2 className="experience-sub-title project-title">{title}</h2>
-    <div className="btn-container">
-      {/* <button className="btn btn-color-2 project-btn" onClick={() => window.open(githubLink)}>
+    {/* <div className="btn-container">
+      <button className="btn btn-color-2 project-btn" onClick={() => window.open(githubLink, '_blank')}>
         Github
       </button>
-      <button className="btn btn-color-2 project-btn" onClick={() => window.open(liveDemoLink)}>
+      <button className="btn btn-color-2 project-btn" onClick={() => window.open(liveDemoLink, '_blank')}>
         Live Demo
-      </button> */}
-    </div>
+      </button>
+    </div> */}
   </div>
 );
 
 const Projects = () => (
   <section id="projects">
-    {/* <p className="section__text__p1">Browse My Recent</p> */}
     <h1 className="title">Projects</h1>
     <div className="experience-details-container">
       <div className="about-containers">
@@ -59,12 +59,6 @@ const Projects = () => (
         ))}
       </div>
     </div>
-    {/* <img
-      src="./assets/arrow.png"
-      alt="Arrow icon"
-      className="icon arrow"
-      onClick={() => window.location.href = './#contact'}
-    /> */}
   </section>
 );
 
